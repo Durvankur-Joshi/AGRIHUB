1d8e685ce4d3e93c93d9690fea7e547f11809012
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-11-29 15:10:42 +0530

    order and product page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import FarmerLogin from './Farmer/FarmerLogin.jsx'
 import FarmerSignup from './Farmer/FarmerSignup.jsx'
 import FarmerDashboard from './Farmer/FarmerDashboard.jsx'
+import Orders from './Farmer/Orders'
+import MyProduct from './Farmer/MyProduct'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,8 +19,12 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path = '/farmerlogin' element = {<FarmerLogin />} />
       <Route  path="/farmer-signup" element={<FarmerSignup />} />
-      <Route path='farmerdashboard'
+      <Route path='/farmerdashboard'
       element ={<FarmerDashboard/>}/>
+      <Route path='/farmerdashboard/orders'
+      element={<Orders />} />
+      <Route path='/farmerdashboard/myproduct'
+      element={<MyProduct />} />
       </Routes>
     </BrowserRouter>
     </>
--- frontend/src/Farmer/components/ActionButton.jsx
+++ frontend/src/Farmer/components/ActionButton.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import AddProductDialog from './AddProductDialog'
+import MyProduct from '../MyProduct'
+import Orders from '../Orders'
+import { useNavigate } from 'react-router-dom'
 
 const ActionButton = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div
@@ -15,10 +19,13 @@ const ActionButton = () => {
 
         <Button
           className="bg-white text-base font-medium hover:bg-green-200"
-          variant="primary">Orders</Button>
+          variant="primary"
+          onClick={()=> navigate('/farmerdashboard/orders')}
+          >Orders</Button>
         <Button
           className="bg-white text-base font-medium hover:bg-green-200"
-          variant="primary">My Products</Button>
+          variant="primary"
+          onClick={() => navigate('/farmerdashboard/myproduct')}>My Products</Button>
 
       </div>
     </>
